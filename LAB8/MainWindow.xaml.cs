ac6e642ed2d0e4f8d4cbb192281eeef2630ca02f
Natalliax <Natalliax@NATALLIAX>
2024-02-23 20:23:56 +0300

    Добавьте файлы проекта.

---
+++ LAB8/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using LAB8.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.Entity.Core.Objects.DataClasses;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       EntityContext context; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new EntityContext();
+            context.Students.Load();
+            dGrid.ItemsSource = context.Students.Local.ToBindingList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student();
+            EditWindow editWindow = new EditWindow(student);
+            if (editWindow.ShowDialog()==true)
+            {
+                context.Students.Add(student);
+                context.SaveChanges();
+                editWindow.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (dGrid.SelectedItems.Count == 0)
+                return;
+            if (MessageBox.Show("Вы уверены?", "Удалить запись?", MessageBoxButton.YesNo) == MessageBoxResult.No)
+                return;
+                for(int i = dGrid.SelectedItems.Count - 1; i>=0; i--)
+            {
+                context.Students.Remove(dGrid.SelectedItems[i] as Student);
+            }
+            context.SaveChanges();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if(dGrid.SelectedItems.Count == 0) return;
+            EditWindow editWindow = new EditWindow(dGrid.SelectedItem as Student);
+            if (editWindow.ShowDialog() == true)
+                context.SaveChanges();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            context.Dispose();
+        }
+
+        private void dGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex() + 1;
+        }
+    }
+}
---
+++ LAB8/Models/EntityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB8.Models
+{
+    internal class EntityContext : DbContext
+    {
+        public EntityContext(): base ("DefaultConnection")
+        {
+            Database.SetInitializer(new DataBaseInitializer());
+
+        }
+        public DbSet <Student> Students { get; set; }
+    }
+}
